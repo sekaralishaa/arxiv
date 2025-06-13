288ddd71e85e615c422765f9a63d891164211d8c
Sekar Alisha <106976018+sekaralishaa@users.noreply.github.com>
2025-06-13 23:09:06 +0700

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -117,7 +117,7 @@ Sistem rekomendasi ini hanya menyajikan artikel dalam bidang <b>Fisika</b>, <b>M
 
 # Input fields
 st.markdown("#### 📝 Judul Artikel")
-title = st.text_input("Masukkan judul lengkap atau kata kunci utama dari artikel (Contoh:Image Classification Using Deep Neural Network")
+title = st.text_input("Masukkan judul lengkap atau kata kunci utama dari artikel (contoh:Image Classification Using Deep Neural Network)")
 # st.caption("Contoh: *Image Classification Using Deep Neural Network*")
 
 st.markdown("#### 🔑 Keyword (pisahkan dengan koma)")
